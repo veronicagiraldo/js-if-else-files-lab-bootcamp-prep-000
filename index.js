c94d637850f9978db4f77bd271ed2ca5372a7c95
Veronica Giraldo <veronica9893@gmail.com>
2018-01-12 01:58:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
+<<<<<<< HEAD
 var name = 'Susan';
 var height = 70
+=======
+var name = "Joe"
+var height = "74"
+var index = require(".index.js")
+console.log(index.name)
+
+
+>>>>>>> dffed6796d262f46da86a0cce3c23454b42c9b71
 
 // Don't worry about this
 module.exports = { name, height
--- other_file.js
+++ other_file.js
@@ -1,2 +1,6 @@
 var index = require("./index.js")
+<<<<<<< HEAD
 console.log(name)
+=======
+console.log(index.name)
+>>>>>>> dffed6796d262f46da86a0cce3c23454b42c9b71
